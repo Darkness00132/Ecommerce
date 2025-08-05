546278e9601325c16497547965763edd0749a505
Darkness00132 <mustafa54862@gmail.com>
2025-08-05 13:20:25 +0300

    add ar

--- frontend/src/axiosInstance/axiosInstance.js
+++ frontend/src/axiosInstance/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import useAuthUser from "../store/useAuthUser";
+import useCart from "../store/useCart";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -12,6 +13,7 @@ axiosInstance.interceptors.response.use(
     if (error.response?.status === 401) {
       // Unauthorized = token expired or invalid
       useAuthUser.getState().clearUser();
+      useCart.getState().clearCart();
       return Promise.reject(error);
     }
   }
